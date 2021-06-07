4fcd30fb079c2f7d1ee01aea55d0508adea0f3e6
Eric Vuu <ericlv91@gmail.com>
2021-06-07 15:50:15 -0700

    added another svg-text-animation

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 let fullname = document.querySelectorAll('#fullname path')
 
-for(let i =0; i < localStorage.length; i++){
+for(let i =0; i <fullname.length; i++){
   console.log(`Letter ${i} is ${fullname[i].getTotalLength()}`);
 }
+
+let position = document.querySelectorAll('#position path')
+
+for(let i =0; i < position.length; i++){
+  console.log(`Letter ${i} is ${position[i].getTotalLength()}`);
+}
+
+console.log(position)
